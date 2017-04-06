36577d3fcdfa613ef75bffe279252483e67a9480
Daniel Freiling <dfg@nota.dk>
2017-04-06 17:33:55 +0200

    fix custom android activity

--- app/activity.android.ts
+++ app/activity.android.ts
@@ -1,21 +1,57 @@
+import { EventEmitter } from '@angular/core';
 import {setActivityCallbacks, AndroidActivityCallbacks} from 'ui/frame';
 
+export var OnRouteToURL = new EventEmitter<string>();
+
 @JavaProxy('com.my.company.MainActivity')
-class Activity extends android.app.Activity {
+export class Activity extends android.app.Activity {
 
   private _callbacks: AndroidActivityCallbacks;
 
   protected onCreate(savedInstanceState: android.os.Bundle): void {
-    if (!this._callbacks) {
-      setActivityCallbacks(this);
-    }
-    this._callbacks.onCreate(this, savedInstanceState, super.onCreate);
-    console.info(`MainActivity.onCreate`);
+      if (!this._callbacks) {
+          setActivityCallbacks(this);
+      }
+
+      this._callbacks.onCreate(this, savedInstanceState, super.onCreate);
+      console.info(`MainActivity.onCreate`);
+  }
+
+  protected onSaveInstanceState(outState: android.os.Bundle): void {
+      this._callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
+  }
+
+  protected onStart(): void {
+      this._callbacks.onStart(this, super.onStart);
+  }
+
+  protected onStop(): void {
+      this._callbacks.onStop(this, super.onStop);
+  }
+
+  protected onDestroy(): void {
+      this._callbacks.onDestroy(this, super.onDestroy);
+  }
+
+  public onBackPressed(): void {
+      this._callbacks.onBackPressed(this, super.onBackPressed);
+  }
+
+  public onRequestPermissionsResult(requestCode: number, permissions: Array<String>, grantResults: Array<number>): void {
+      this._callbacks.onRequestPermissionsResult(this, requestCode, permissions, grantResults, undefined /*TODO: Enable if needed*/);
+  }
+
+  protected onActivityResult(requestCode: number, resultCode: number, data: android.content.Intent): void {
+      this._callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
   }
 
   protected onNewIntent(intent: android.content.Intent): void {
     super.onNewIntent(intent);
-    const intentData = intent.getData();
-    console.info(`MainActivity.onNewIntent with data: ${intentData}`);
+    console.info(`MainActivity.onNewIntent with data: ${intent.getData()}`);
+    
+    if (intent.getAction() === android.content.Intent.ACTION_VIEW) {
+      const dataStr = intent.getDataString();
+      OnRouteToURL.next(dataStr);
+    }
   }
 }
